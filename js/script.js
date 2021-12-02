b78cf4feb4f1e3e9d491fcdadce62cf60a4dd17f
Gozzissimo <fantini.nico@gmail.com>
2021-12-02 12:24:24 +0100

    rifacimento codice

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// Consegna
-// L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
-// con difficoltà 1 => tra 1 e 100
-// con difficoltà 2 => tra 1 e 81
-// con difficoltà 3 => tra 1 e 49
-// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
-// Consigli del giorno: : party_wizard:
-// Facciamo prima la parte grafica a mano e solo dopo passiamo allo script
-// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-// Ad esempio:
-// Di cosa ho bisogno per generare i numeri ?
-//     Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-// Le validazioni e i controlli possiamo farli in un secondo momento.
-
 const play = document.querySelector('.play');
 const grid = document.querySelector('.grid');
 const select = document.getElementById('difficulty');
@@ -19,16 +5,26 @@ let numMin = 1;
 let numMax = 100;
 
 
-//tracciare il cambio di livello
-select.addEventListener("change", function () {
+click sul pulsante play
+play.addEventListener('click', function(){
+
     let level = select.value;
-    console.log(level);
+
+    for (let i = numMin; i <= numMax; i++) {
+        
+        
+    }
+
+    //in base al livello sceglgo il numero di celle
+    if (level == 1) {
+        
+    }
+
+
+    //vado a creare il numero di celleche mi serve
+
+
+
+    //click sulle celle
 
 });
-
-//in base al livello sceglgo il numero di celle
-
-
-//vado a creare il numero di celleche mi serve
-
-//click sulle celle
